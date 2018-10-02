a3ec1f7e693987ed7c55b00e35e0c2d8d7018e8c
Walker Reynolds <james.reynolds.14@cnu.edu>
2018-10-02 18:27:29 -0400

    Fixed issue with bad npm module breaking the project

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,10 +9,9 @@ import Axios from 'axios'
 import '../Styles/App.css';
 import 'react-slide-out/lib/index.css'
 
-const uws = require('uws')
-const socketIO = require('socket.io')
+import openSocket from 'socket.io-client';
 
-
+const socket = openSocket(apiEndpoint);
 
 var video = ''
 const youtubeAPIKey = 'AIzaSyD7edp0KrX7oft2f-zL2uEnQFhW4Uj5OvE'
