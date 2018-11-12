0064b434bc7a332623f509822c57ff83e0fb747c
Walker Reynolds <james.reynolds.14@cnu.edu>
2018-11-12 01:31:13 -0500

    Added checking version to get into the site

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import App from './App'
 import Login from './Login'
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Axios from 'axios'
+
+// const apiEndpoint = 'http://127.0.0.1:5000'
+const apiEndpoint = 'https://plug-dj-clone-api.herokuapp.com'
 
 export default class Home extends Component {
   constructor(props) {
@@ -10,7 +14,33 @@ export default class Home extends Component {
     this.state = {
       loggedIn: false,
       username: '',
+      version: '0.101',
+      disableLoginButton: false
     }
+  }
+
+  componentDidMount(){
+    this.checkVersion()
+  }
+
+  checkVersion = () => {
+    var url = apiEndpoint + '/getCurrentVersion'
+    
+
+    Axios.get(url)
+      .then((response) => {  
+        console.log(response)      
+        var currentVersion = response['data']['version']
+
+        if(currentVersion !== this.state.version){
+          this.setState({
+            disableLoginButton: true
+          })
+        }
+        
+      })
+
+
   }
 
   changeUsername = (val) => {
@@ -40,7 +70,7 @@ export default class Home extends Component {
 
 
             <Route exact path='/' render={
-              (props) => this.state.loggedIn ? (<Redirect to="/home" />) : (<Login {...props} changeUsername={this.changeUsername} changeLoggedIn={this.changeLoggedIn} />)
+              (props) => this.state.loggedIn ? (<Redirect to="/home" />) : (<Login {...props} changeUsername={this.changeUsername} changeLoggedIn={this.changeLoggedIn} disableLoginButton={this.state.disableLoginButton} />)
             } />
 
           </div>
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -19,8 +19,10 @@ export default class Login extends Component {
     };
   }
 
+  
+
   validateForm() {
-    return this.state.username.length > 0 && this.state.password.length > 0;
+    return this.state.username.length > 0 && this.state.password.length > 0 && !this.props.disableLoginButton;
   }
 
   handleChange = event => {
@@ -51,6 +53,8 @@ export default class Login extends Component {
           this.props.changeLoggedIn(true)
 
           this.forceUpdate()
+        }else if(response.data === 'fuck you'){
+          alert('Fuck you, stop trying to ruin my database')
         }else{
           alert('Your password was wrong, dingus')
         }
@@ -60,34 +64,48 @@ export default class Login extends Component {
 
   render() {
     return (
-      <div className="Login">
-        <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="username" bsSize="large">
-            <ControlLabel>Username</ControlLabel>
-            <FormControl
-              autoFocus
-              type="text"
-              value={this.state.username}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-            />
-          </FormGroup>
-          <Button
-            block
-            bsSize="large"
-            disabled={!this.validateForm()}
-            type="submit"
-          >
-            Login
-          </Button>
-        </form>
+      
+      
+
+      <div>
+        {!this.props.disableLoginButton && 
+          <div className="Login">
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="username" bsSize="large">
+              <ControlLabel>Username</ControlLabel>
+              <FormControl
+                autoFocus
+                type="text"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup controlId="password" bsSize="large">
+              <ControlLabel>Password</ControlLabel>
+              <FormControl
+                value={this.state.password}
+                onChange={this.handleChange}
+                type="password"
+              />
+            </FormGroup>
+            <Button
+              block
+              bsSize="large"
+              disabled={!this.validateForm()}
+              type="submit">
+
+              Login
+            </Button>
+          </form>
+        </div>
+        }
+
+        {this.props.disableLoginButton &&
+          <div style={{'margin': 'auto', 'width': '50%', 'border': '3px solid white', 'padding': '10px', 'marginTop':'20%', 'color':'white'}}>
+            <h1>You do not have the latest version of the website, please hard refresh(ctrl + shift + R). If this does not work, close your browser and reopen.</h1>
+          </div>
+        }
+        
       </div>
     );
   }
